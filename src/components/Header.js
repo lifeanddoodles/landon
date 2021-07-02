1933842894ab0d9105b8928e1d51824ec9552d11
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2021-07-02 14:05:42 -0700

    Make components dynamic, added JSON to represent data.

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import menuLinksData from '../data/menu_links.json'
+
+const Header = () => {
+  return (
+    <header id="intro">
+    <article className="fullheight">
+      <div className="hgroup">
+        <h1>Landon Hotel</h1>
+        <h2>West London</h2>
+        <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+      </div>
+    </article>
+
+    <nav id="nav">
+      <div className="navbar">
+        <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+        <ul>
+          { menuLinksData.map(menuLink =>
+          <li><a className={`icon ${menuLink.class}`} href={menuLink.href}><span>{menuLink.text}</span></a></li>) }
+        </ul>
+      </div>
+    </nav>
+    </header>
+  )
+}
+
+export default Header
